f7ed0122df69d9e5236880e0583bc7cbe16fc35e
unknown <kineida1@mail.ru>
2023-06-04 20:03:38 +0300

    Выполнение дз

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+int num = Convert.ToInt32(number);
+int sum = 0; 
+for (int i = 1; i <= len; i++) {
+    sum += num % 10;
+    num /= 10; 
+}
+Console.WriteLine($"сумма числа: {number} равно: {sum}");
---
+++ Zadacha29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Нажмите Enter для создания массива и 8ми элементов");
+int number = 8;
+int[] array = new int[number];
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(number);
+    Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
